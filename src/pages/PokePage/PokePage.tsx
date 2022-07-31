deed9c9ae02ca222db00f17f5c3aa70334758910
unnclear <danilyudin723@gmail.com>
2022-07-31 17:15:02 +0600

    styles

--- src/pages/PokePage/PokePage.tsx
+++ src/pages/PokePage/PokePage.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 
+import cn from 'classnames';
+
 import { useRequestPokemonsQueries } from '../../utils/api/hooks/pokemon';
 
 import styles from './PokePage.module.scss';
 
 export const PokePage = () => {
-  const [offset, setOffset] = useState(10);
+  const [offset, setOffset] = useState(3);
+
+  const [selectedPokemonID, setSelectedPokemonID] = useState(1);
 
   const results = useRequestPokemonsQueries({ offset });
   const isLoading = results.some((results) => results.isLoading);
@@ -13,23 +17,48 @@ export const PokePage = () => {
   if (isLoading) return null;
 
   const pokemons = results.map((result: any) => result.data.data);
-  console.log(pokemons);
+
+  const selectedPokemon = pokemons.find((pokemon) => selectedPokemonID === pokemon.id)!;
+
+  console.log(selectedPokemon);
 
   return (
-    <div className={styles.page_container}>
-      <div className={styles.content_container}>
-        <div>card</div>
-        <ul className={styles.list_container}>
-          {pokemons.map((pokemon) => (
-            <li key={pokemon.id} className={styles.pokemon_item}>
-              <img
-                className={styles.pokemon_item_image}
-                src={pokemon.sprites.front_default}
-                alt='pokemon_sprite_item'
-              />
-              {pokemon.name}
-            </li>
-          ))}
+    <div className={styles.page}>
+      <div className={styles.content}>
+        <div className={styles.card}>
+          <div className={styles.card_title}>
+            <div className={styles.card_title_name}>{selectedPokemon.name}</div>
+            <div className={styles.card_title_id}>#000{selectedPokemon.id}</div>
+          </div>
+          <div>image</div>
+          <div>statistic</div>
+        </div>
+        <ul className={styles.list}>
+          {pokemons.map((pokemon) => {
+            const isActive = selectedPokemonID === pokemon.id;
+            return (
+              <li
+                key={pokemon.id}
+                role='option'
+                aria-selected={isActive}
+                className={cn(styles.pokemon_item, {
+                  [styles.pokemon_item_active]: isActive
+                })}
+                tabIndex={0}
+                onKeyPress={(e) => {
+                  if (e.key === 'Enter') setSelectedPokemonID(pokemon.id);
+                }}
+                onClick={() => setSelectedPokemonID(pokemon.id)}
+              >
+                <img
+                  className={styles.pokemon_item_image}
+                  src={pokemon.sprites.front_default}
+                  alt='pokemon_sprite_item'
+                />
+                {pokemon.name}
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
